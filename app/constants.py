33c8a7847b44ef4633cc638e847f86777108933b
Jurupoc <jurupoc222@gmail.com>
2024-01-20 14:23:54 -0300

    Creation of a simple HTML template with a CSS. Creation of a Constant File.

---
+++ app/constants.py
@@ -0,0 +1,8 @@
+# PATHS
+BASE_FOLDER = "app"
+TEMPLATES_PATH = f"{BASE_FOLDER}/templates"
+WELCOME_PAGE_TEMPLATE_PATH = f"{TEMPLATES_PATH}/welcome_page.html"
+
+
+# FILES
+WELCOME_PAGE = "welcome_page.html"
